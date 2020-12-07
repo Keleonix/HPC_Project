7de8d4456991dbcde468d6a254bf31862f1a94ea
Keleonix <axel@arctique.fr>
2020-12-07 09:02:56 +0100

    chaine_simd a jour

--- src/main.c
+++ src/main.c
@@ -89,33 +89,33 @@ int main(int argc, char const *argv[]){
 
         case 2:
             /*Chrono étapes SD */
-            chrono_SD_step1(n);
-            chrono_SD_step2(n);
-            chrono_SD_step3(n);
-            chrono_SD_step4(n);
+            // chrono_SD_step1(n);
+            // chrono_SD_step2(n);
+            // chrono_SD_step3(n);
+            // chrono_SD_step4(n);
 
             /*Chrono étapes SD SIMD*/
-            chrono_SD_step1_SIMD(n);
-            chrono_SD_step2_SIMD(n);
-            chrono_SD_step3_SIMD(n);
-            chrono_SD_step4_SIMD(n);
+            // chrono_SD_step1_SIMD(n);
+            // chrono_SD_step2_SIMD(n);
+            // chrono_SD_step3_SIMD(n);
+            // chrono_SD_step4_SIMD(n);
 
             /*Chrono SD optimisé*/
-            chrono_SD_steps_OPTIM(n);
+            // chrono_SD_steps_OPTIM(n);
 
             /*Chrono morpho */
-            chrono_erosion(n);
-            chrono_erosion_SIMD(n);
-            chrono_erosion_OPTIM(n);
+            // chrono_erosion(n);
+            // chrono_erosion_SIMD(n);
+            // chrono_erosion_OPTIM(n);
 
-            chrono_dilatation(n);
-            chrono_dilatation_SIMD(n);
+            // chrono_dilatation(n);
+            // chrono_dilatation_SIMD(n);
             // chrono_dilatation_OPTIM(n);
 
             /* Chrono chaine complète*/
-            chrono_chaine(n);
+            // chrono_chaine(n);
             chrono_chaine_SIMD(n);
-            chrono_chaine_OPTIM(n);
+            // chrono_chaine_OPTIM(n);
             break;
     }
 
